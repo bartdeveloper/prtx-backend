d813df2593b71a5517d2243a2345226f22da3975
Bartek <bartek@appselio.pl>
2019-01-08 17:50:03 +0100

    Added Search Engine.

---
+++ prtx_backend/Core/SearchEngine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace prtx_backend.Core
+{
+    public class SearchEngine
+    {
+        public async Task<IEnumerable<Product>> searchProducts(string query)
+        {
+
+            PRTXEntities db = new PRTXEntities();
+
+            var products = await db.Products.Where(p => p.name.Contains(query) || p.description.Contains(query)).ToListAsync();
+
+            return products;
+
+        }
+    }
+}
